9cc97eacc1297897521ae176c3092682eb1314de
dinosawrusrex <dennissaw12@gmail.com>
2021-12-04 01:17:53 -0800

    Try calculating score when checking if number in card, pop when hit
    If a score is returned, that's a bingo!
    Fancy try/except is cool with the custom Bingo exception, but meh..
    
    No need to override the contains. Just have the method that check is
    bingo handle if number is in the card, has a hit, and calculate score if
    there's a bingo.
    
    Pop from numbers dict, no need to keep track of what's been hit
    separately. Easier to calculate the sum of unhits for the score!

--- day4.py
+++ day4.py
@@ -1,37 +1,24 @@
 import collections
 
 
-class Bingo(Exception):
-    pass
-
-
 class Board:
 
     def __init__(self):
         self.numbers = {}
-        self.hit = set()
         self.x_hit = collections.defaultdict(lambda: 0)
         self.y_hit = collections.defaultdict(lambda: 0)
 
-    def __contains__(self, number):
+    def is_bingo(self, number):
         if number in self.numbers:
-            self.hit.add(number)
-            self.update_hit(number)
+            x, y = self.numbers.pop(number)
+            self.x_hit[x] += 1
+            self.y_hit[y] += 1
+            if self.x_hit[x] == 5 or self.y_hit[y] == 5:
+                return sum(map(int, self.numbers)) * int(number)
 
-    def update_hit(self, number):
-        x, y = self.numbers[number]
-        self.x_hit[x] += 1
-        self.y_hit[y] += 1
-        if self.x_hit[x] == 5 or self.y_hit[y] == 5:
-            raise Bingo(f'{self.score(number)}')
             
-    def score(self, last_hit):
-        return sum(map(int, set(self.numbers).difference(self.hit))) * int(last_hit)
-
-
 def parse_input(input_file):
-    draw = []
-    boards = []
+    draw, boards = [], []
     with open(input_file) as f:
         y = 0
         for l in f:
@@ -50,25 +37,18 @@ def simulate(draw, boards):
     first, win_sequence = None, set()
     for n in draw:
         for board in boards:
-            try:
-                n in board
-            except Bingo as e:
+            if (score := board.is_bingo(n)):
                 if not first:
-                    first = str(e)
+                    first = score
                 win_sequence.add(board)
                 if len(win_sequence) == len(boards):
-                    return first, str(e)
-    return '', ''
+                    return first, score
+    return None, None
 
 
 if __name__ == '__main__':
     draw, boards = parse_input('inputs/day4test.txt')
-    first, last = simulate(draw, boards)
-    assert '4512' in first
-    assert '1924' in last
+    assert (4512, 1924) == simulate(draw, boards)
 
     draw, boards = parse_input('inputs/day4.txt')
-    first, last = simulate(draw, boards)
-    assert '50008' in first
-    assert '17408' in last
-
+    assert (50008, 17408) == simulate(draw, boards)
